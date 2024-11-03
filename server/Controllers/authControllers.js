07cc91fea9d4b66647585037d365f12bbb7f9d22
Chamal-Dakshana <chamaldhak0456@gamil.com>
2024-11-03 12:25:35 +0530

    Encrypt the password using bcrypt. Add new page auth.js & Update the AuthController.js

--- server/Controllers/authControllers.js
+++ server/Controllers/authControllers.js
@@ -1,4 +1,5 @@
 const Student = require('../models/student')
+const {hashPassword, comparePassword} = require("../helpers/auth")
 
 const test= (req, res) => {
     res.json('Test is working')
@@ -11,25 +12,28 @@ const registerStudent = async(req, res) => {
         if(!name) {
             return res.json({
                 error: 'name is required'
-            })
-        };
+            });
+        }
         // Check if  password is good
         if(!password || password.lenght > 6) {
             return res.json({
-                error: 'Password is required or should be at least 6 character long'
-            })
-        };
+                error: 'Password is required or should be at least 6 character long',
+            });
+        }
         // Check email
         const exist = await Student.findOne({email});
         if(exist){
             return res.json({
                 error: 'Email is taken already'
-            })
+            });
         }
+        const hashedPassword = await hashPassword(password)
 
         const student = await Student.create({
-            name, email, password
-        }) 
+            name, 
+            email, 
+            password:hashedPassword,
+        }); 
 
         return res.json(student)
     }catch(error) {
